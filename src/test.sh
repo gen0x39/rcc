59ea3fc05395d6ceb423ac13101893d90eae9b8d
gen0x39 <migesan0319@gmail.com>
2020-02-01 11:30:10 +0900

    Compile a single number to a program that exits with the given number.

---
+++ src/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+
+  cargo run "$input" >> tmp.s
+
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+  rm -f tmp*
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
